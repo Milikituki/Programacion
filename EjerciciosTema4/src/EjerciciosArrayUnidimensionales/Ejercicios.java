04c93f0186a94c5c0fda408f969982389eb44ac3
Miriam Fernández <myriam1869@gmail.com>
2025-11-23 13:14:31 +0100

    ejercicio 4, buscar numero dentro del array

--- EjerciciosTema4/src/EjerciciosArrayUnidimensionales/Ejercicios.java
+++ EjerciciosTema4/src/EjerciciosArrayUnidimensionales/Ejercicios.java
@@ -1,5 +1,7 @@
 package EjerciciosArrayUnidimensionales;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicios {
@@ -26,13 +28,37 @@ public class Ejercicios {
         String[] nombres = new String[5];
         for (int i = 0; i < nombres.length; i++) {
             System.out.printf("Introduce el nombre %d: ", i+1);
-            String nombre = "";
+            String nombre = ""; // creas una variable "individual" donde se va a guardar un nombre
             nombre = scanner.nextLine();
-            nombres[i] = nombre;
+            nombres[i] = nombre; // dentro del array guardas cada nombre individual en la posicion que le toque
         }
         System.out.println("Nombres introducidos: ");
         for (String item : nombres) {
             System.out.println(item);
+        }
+    }
+    public void ejercicio4(){
+        /*Crea un programa con un array de 7 números. Pide al usuario un número a buscar y usa un bucle for para recorrer el array. Si el número existe, muestra su posición. Si no existe, indica que no se encontró.*/
+
+        int[] numero = new int[7];
+        for (int i = 0; i < numero.length; i++) {
+            int numeroIndividual = (int) (Math.random()*25)+1;
+            numero[i] = numeroIndividual;
+        }
+        System.out.printf("Array: %s%n", Arrays.toString(numero));
+        System.out.printf("Introduce el número que quieras buscar: ");
+        int numeroBuscar = scanner.nextInt();
+        boolean encontrado = false;
+        for (int i = 0; i<numero.length; i++) {
+            if (numeroBuscar == numero[i]){
+                System.out.printf("El número %d se encuentra en la posición %d", numeroBuscar, i);
+                encontrado = true;
+                break;
+            }
+            if (!encontrado){
+                System.out.printf("El número %d no se encuentra en el array", numeroBuscar);
+                break;
+            }
         }
     }
 
--- EjerciciosTema4/src/EjerciciosArrayUnidimensionales/Main.java
+++ EjerciciosTema4/src/EjerciciosArrayUnidimensionales/Main.java
@@ -3,7 +3,7 @@ package EjerciciosArrayUnidimensionales;
 public class Main {
     public static void main(String[] args) {
         Ejercicios e = new Ejercicios();
-        e.ejercicio3();
+        e.ejercicio4();
 
     }
 }
